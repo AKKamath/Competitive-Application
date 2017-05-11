027e6e003e3f5708af35898f85ba6ad64c94d962
AdityaK <a_kamath@hotmail.com>
2017-05-11 14:18:27 +0530

    Add constructor for contest class

--- models/m_contest.php
+++ models/m_contest.php
@@ -1,10 +1,19 @@
 <?php
 	class contest
 	{
-		var $start;
-		var $end;
-		var $img;
-		var $site_name;
-		var $contest_name;
+		var $name_site;
+		var $img_site;
+		var $name_contest;
+		var $date_start;
+		var $date_end;
 		var $description;
+		contest($a, $b, $c, $d, $e, $f, $g)
+		{
+			$name_site = $a;
+			$img_site = $b;
+			$name_contest = $c;
+			$date_start = $d;
+			$date_end = $e;
+			$description = $f;
+		}
 	}
