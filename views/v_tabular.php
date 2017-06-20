1f2dc93354faa289cc21af0c40b0694eabac2a68
AdityaK <a_kamath@hotmail.com>
2017-06-20 18:09:13 +0530

    Link contest name to contest page

--- views/v_tabular.php
+++ views/v_tabular.php
@@ -15,16 +15,18 @@
 				</tr>
 			</thead>
 			<tbody>
+				<div id = "tab_data">
 				<?php foreach($contests as $contest): ?>
 				<tr>
 					<td><img src = "<?php print $contest['img_site']              ?>" width = 20 height = 20></td>
 					<td><?php echo $contest['name_site']                          ?></td>
-					<td><?php echo $contest['name_contest']                       ?></td>
+					<td><a href = <?php echo $contest['site']?>><?php echo $contest['name_contest']?></a></td>
 					<td><?php echo date("M d Y H:i:s", $contest['date_start']);   ?></td>
 					<td><?php echo date("M d Y H:i:s", $contest['date_end']);     ?></td>
 					<td><?php echo $contest['description']                        ?></td>
 				</tr>
 				<?php endforeach; ?>
+				</div>
 			</tbody>
 		</table>
 	</body>
