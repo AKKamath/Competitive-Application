d1e00461993541ba13e62189c980ad26f99c2086
AdityaK <a_kamath@hotmail.com>
2017-08-11 14:19:23 +0530

    Add CodeForces scraping

--- controllers/c_codeforces.php
+++ controllers/c_codeforces.php
@@ -19,12 +19,20 @@
 		}
 		preg_match("/<div class=\"datatable\"[^>]*?>([\s\S]*?)<div class=\"datatable\"/", $site_data, $site_data);
 		$site_data = $site_data[1];
-		preg_match_all("/<tr>([\S\s]*?)<\/tr>/", $site_data, $site_data);
+		//echo $site_data;
+		preg_match_all("/<tr[^d]*?(data-contestId=\"[\S\s]*?)<\/tr>/", $site_data, $matches);
+		
 		// Iterate through competitions
 		foreach ($matches[1] as $match)
 		{
+			preg_match("/data-contestId=\"(.*?)\"/", $match, $url);
+			preg_match("/<td>[\r\n]*([\S ]*)<\/td>/", $match, $title);
+			preg_match("/\"format-time\"[^>]*?>(.*?)<\/span>/", $match, $date_start);
+			preg_match("/month=(.*?)&/", $match, $month);
+			$date_start = preg_replace("/^\w+/", $month[1], $date_start[1]);
+			echo $date_start;
 			// Add details to array
-			$arr[] = new contest("CodeForces", "/images/CodeForces.png", $title[1], $date_start[1], $date_end[1], $details[1], "" . $match);		
+			$arr[] = new contest("CodeForces", "/images/CodeForces.png", $title[1], strtotime($date_start), $date_end[1], $details[1], "http://codeforces.com/contests/" . $url[1]);		
 		}
 		
 		// No details found!
