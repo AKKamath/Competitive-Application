135ec51833b7a45a161dce54dc8ee027fc94d90d
AdityaK <a_kamath@hotmail.com>
2017-08-15 18:00:47 +0530

    Fix codechef error with title

--- controllers/c_codechef.php
+++ controllers/c_codechef.php
@@ -35,7 +35,7 @@
 		{
 			$match = $matches[1][$i];
 			// Add details to array
-			$arr[] = new contest("CodeChef", "/images/CodeChef.jpg", $title[1], strtotime($dates_start[1][$i]), strtotime($dates_end[1][$i]), "Click link to view details", "https://www.codechef.com" . $match);		
+			$arr[] = new contest("CodeChef", "/images/CodeChef.jpg", $name[1][$i], strtotime($dates_start[1][$i]), strtotime($dates_end[1][$i]), "Click link to view details", "https://www.codechef.com" . $match);		
 		}
 		
 		// No details found!
